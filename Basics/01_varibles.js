7f64a990e03f93feaf04cd245e0303e14ad26b21
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2023-09-21 10:28:01 +0000

    Data Types

---
+++ Basics/01_varibles.js
@@ -0,0 +1,14 @@
+const accountId= 10;
+let accountEmail= "jeenesh.iet2017@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lucknow";
+
+
+/*  Prefer not to use const 
+  
+
+*/
+
+accountId=12;
+console.log(accountId);
+console.table([accountId ,accountEmail ,accountPassword ,accountCity])
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"  // Treat all js code as latest version
+
+//  alert("Hello")  We are using node js not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
