ef80bd0754397a895d487a5d2ae2793e26c918bd
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2023-09-27 09:18:10 +0000

    Data Types

---
+++ Basics/05dataTypes_summary.js
@@ -0,0 +1,19 @@
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name:"Jeenesh",
+    age:22
+}
+
+const myFunction = function(){
+
+    console.log("Hello World")
+}
+
+myFunction();
+
+
+// Stack (Primitive)  ,Heap (Non-Premitive);
+
+let myYtName ="Somrthing.com"
+
