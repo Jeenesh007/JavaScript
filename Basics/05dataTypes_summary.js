684550e2eae4ab7cbe0cea4748cce9fa7a25a3cd
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2023-10-05 06:19:45 +0000

    Latest

--- Basics/05dataTypes_summary.js
+++ Basics/05dataTypes_summary.js
@@ -1,24 +1,33 @@
-const heros = ["shaktiman","naagraj","doga"];
+//  Primitive
 
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name:"Jeenesh",
-    age:22
+    name: "hitesh",
+    age: 22,
 }
 
 const myFunction = function(){
-
-    console.log("Hello World")
-}
-
-myFunction();
-
-
-// Stack (Primitive)  ,Heap (Non-Premitive);
-
-let myYtName ="Somrthing.com"
-
-
-let userOne = {
-    email:"",
-    upiId:""
+    console.log("Hello world");
 }
