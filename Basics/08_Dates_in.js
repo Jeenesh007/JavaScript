d92763eeefcac1d48102287fadfbd61304ab88d6
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2023-10-05 11:21:10 +0000

    Date and Time

---
+++ Basics/08_Dates_in.js
@@ -0,0 +1,9 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.getMilliseconds());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString())
