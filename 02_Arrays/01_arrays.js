4addd9d8445cbb073d0a751af8ce05faddf16b68
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2023-12-13 11:34:56 +0000

    New

---
+++ 02_Arrays/01_arrays.js
@@ -0,0 +1,20 @@
+
+
+
+const myArr = [0,1,2,3,4,5,6]
+
+console.log(myArr);
+
+const myHeros = ["Shaktiman ","Naagraj"]
+
+
+console.log(myHeros)
+
+
+
+// Array Methods
+
+
+myArr.push(6);
+
+console.log(myArr)
