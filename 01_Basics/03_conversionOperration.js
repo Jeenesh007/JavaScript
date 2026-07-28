122547c2c95dfdb3c2e12bdf69286791fa8d0add
Jeenesh <42601624+Jeenesh007@users.noreply.github.com>
2026-07-28 19:49:23 +0000

    commit

---
+++ 01_Basics/03_conversionOperration.js
@@ -0,0 +1,44 @@
+let score = "33dwedfew"
+
+console.log(typeof score);
+console.log(typeof(score))
+
+
+let vaulueInNumber = Number(score);
+console.log(typeof vaulueInNumber)
+
+console.log(vaulueInNumber)
+
+let someNumber= 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber)
+console.log(typeof (stringNumber))
+
+
+
+//----------------------------------Opertations---------------------------------------
+
+let value =3
+let negValue= -value
+
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Jeenesh"
+let str3 = str1+str2
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+"2"+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+
+
+
